5697b03784918508e30678086d10a3752dbb1a04
YashDhumke <89930129+YashDhumke@users.noreply.github.com>
2024-03-15 10:45:08 +0530

    dotenv file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes  = require('./routes/authRoutes')
 const cookieParser = require('cookie-parser');
+require('dotenv').config()
 const {requireAuth, checkUser} = require("./middleware/authmiddleware");
 
 const PORT = process.env.PORT || 3000;
@@ -17,7 +18,7 @@ app.use(express.static('public'))  // to serve the public files to the server li
 app.use(express.json())      // it takes any json data which come along with the request and it passes it into a javascript object so that we can use it inside the code 
 app.use(cookieParser())
 // database connectivity ..
-mongoose.connect("mongodb+srv://dhumkeyash:%40Yash_007@smoothiescluster.d0qowsq.mongodb.net/?retryWrites=true&w=majority&appName=smoothiesCluster")
+mongoose.connect(process.env.MONGODB_URL)
   .then(
     app.listen(PORT, () => {
       console.log("listning to the port");
